3a0616332b9facb1ead9128f49e8821440fc68d3
Jon Sanders <jonsanders101@aol.com>
2017-11-08 11:41:20 +0000

    pass to ollie

---
+++ note-controller.js
@@ -0,0 +1,8 @@
+// "use strict";
+window.onload = function () {
+
+var element = document.getElementById('app');
+console.log(document.getElementById('app'));
+console.log(element);
+console.log(document);
+};
